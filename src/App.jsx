e56b8d534d87f9693fd5db0abdbd5d40c8b3d9e7
Qadeer Ahmad <qadeerahmad294gb@gmail.com>
2024-07-21 17:00:33 +0500

    add basic variables and function for working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+  const passwordGenerator=useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str+="0123456789"
+    if(charAllowed) str+="!@#$%^&*()_+"
+    for(let i=0; i<=length; i++){
+      let char=Math.floor(Math.random()*str.length+1)
+      pass +=str.charAt(char)
+    }
+    setPassword(pass)
+    
+
+  },[length,numberAllowed,charAllowed,setPassword])
 
   return (
     <>
